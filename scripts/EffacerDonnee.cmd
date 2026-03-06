b718bf55c957b734665f12895c5f45f2c5203528
Fenitra <fenitranyaratra@gmail.com>
2026-03-06 21:02:05 +0300

    [initialisation][debut] creation script

--- scripts/EffacerDonnee.cmd
+++ scripts/EffacerDonnee.cmd
@@ -4,17 +4,17 @@ SET PGDATABASE=notecorrection
 SET PGHOST=localhost
 SET PGPORT=5432
 
-echo Purge imminente de la base de donnees %PGDATABASE%...
+echo Fafana %PGDATABASE%...
 
 :: Utilisation de TRUNCATE avec CASCADE pour vider toutes les tables en ignorant les contraintes d'ordre
 psql -c "TRUNCATE TABLE note_finale, Note, Parametre, Correcteur, Resolution, Candidat, Operateur, Matiere RESTART IDENTITY CASCADE;"
 
 if %ERRORLEVEL% EQU 0 (
     echo.
-    echo Operation accomplie avec succes. Les tables sont vacantes.
+    echo Mety
 ) else (
     echo.
-    echo Une erreur est survenue lors de l'execution du script.
+    echo Misy erreur
 )
 
 pause
